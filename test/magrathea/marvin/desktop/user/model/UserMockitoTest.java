2353cad9dff498563fc4d0befac300f509afcc39
boscalent <boscalent@PredatorIII>
2016-11-28 01:59:00 +0100

    Mockito con un poco más de funcionalidad

--- test/magrathea/marvin/desktop/user/model/UserMockitoTest.java
+++ test/magrathea/marvin/desktop/user/model/UserMockitoTest.java
@@ -1,5 +1,6 @@
 package magrathea.marvin.desktop.user.model;
 
+import magrathea.marvin.desktop.user.dao.UserRole;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -9,19 +10,42 @@ import static org.mockito.Mockito.*;
  *
  * @author boscalent
  */
-
 // Fake Test, only to prove mockito
 // Here mockito isn't necessary, we have control of the code from User
 // It's only a Test
 public class UserMockitoTest {
 
+    private User userMock;
+
+    @Before
+    public void setUp() {
+        userMock = mock(User.class);                       // ARRANGE MOCK CLASS
+    }
+
     @Test
     public void UserHasEditorRol() {
-        User userMock = mock(User.class);
-        when(userMock.getUserRole()).thenReturn("EDITOR");
-        
-        String result = userMock.getUserRole();
+        when(userMock.getUserRole()).thenReturn("EDITOR");      // ARRANGE
 
-        assertThat(result, equalTo("EDITOR"));
+        String result = userMock.getUserRole();                 // ACT
+
+        assertThat(result, equalTo("EDITOR"));                  // ASSERT
+    }
+
+    @Test
+    public void UserHasLegalRol() {
+        // Return a different String
+        when(userMock.getUserRole()).thenReturn("USER", "EDITOR", "ADMINISTRATOR");
+
+        for (int i = 0; i < UserRole.values().length ; i++) {
+
+            boolean result = false;
+            for (UserRole userRole : UserRole.values()) {
+                if (userRole.name().equalsIgnoreCase(userMock.getUserRole())) {
+                    result = true;
+                }
+            }
+            assertThat(result, is(true));
+
+        }
     }
 }
